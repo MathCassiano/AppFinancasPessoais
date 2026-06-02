502e5769131775a4735fee73ed293b96479dd807
Matheus Cassiano <matheuscassiano6467@gmail.com>
2026-06-02 11:52:48 -0300

    Criando tela de SignIn e SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import { Platform } from 'react-native';
+
+import {
+    AreaInput,
+    Background,
+    Container,
+    Input,
+    SubmitButton,
+    SubmitText } from '../SignIn/styles'
 
 export default function SignUp(){
     return (
-        <View>
-            <Text>Tela Login</Text>
-        </View>
+        <Background>
+            <Container
+              behavior={Platform.OS === 'ios' ? 'padding': ''}
+              enabled
+            >
+                <AreaInput>
+                    <Input
+                        placeholder='Nome'
+                    />
+                </AreaInput>
+                <AreaInput>
+                    <Input
+                        placeholder='Seu e-mail'
+                    />
+                </AreaInput>
+                <AreaInput>
+                    <Input
+                        placeholder='Sua senha'
+                    />
+                </AreaInput>
+
+                <SubmitButton>
+                    <SubmitText>Cadastrar</SubmitText>
+                </SubmitButton>
+
+            </Container>
+        </Background>
     )
 }
